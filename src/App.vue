5296681af69a23215695b9df134da35a3c3e59b8
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2024-01-22 18:56:37 +0300

    feat: add initial overlay

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const overlay = ref(true);
+
+setTimeout(() => {
+  overlay.value = false;
+}, 2000);
+</script>
 
 <template>
-  <v-container class="wrapper">
-    <router-view></router-view>
-  </v-container>
+  <v-overlay scroll-strategy="block" class="overlay" v-model="overlay">
+    <div class="text-h4">Essey</div>
+  </v-overlay>
+  <v-layout class="wrapper">
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
 </template>
 
 <style scoped lang="scss">
 #app {
   min-height: 100vh;
+}
+
+.overlay {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #999999;
 }
 </style>
